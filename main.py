2804ee020763176e2eb5bd015ae0e9222ef2be65
PapstJL4U <PapstJL4U@users.noreply.github.com>
2022-08-31 14:41:42 +0200

    added panda for science

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """find the +onBlock moves in GG Strive and find the percentage for normals and specials
     to do so we need to scrap dustloop.com for each characters moves and do some math
 """
+import website_fiddling as wf
+import dustloop_links as dl
 
 
 if __name__ == "__main__":
-    pass
+    wf.find_Moves(dl.I_No)
--- website_fiddling.py
+++ website_fiddling.py
@@ -1,6 +1,42 @@
 """find the data in the website tables"""
+from statistics import NormalDist
+from collections import OrderedDict
+from typing import OrderedDict
 from bs4 import BeautifulSoup
+import requests
 
-def find_Moves(website:str)->list[str]:
-    with open(website) as fp:
-        soup = BeautifulSoup(fp)
+def find_Moves(website:str):
+        text = requests.get(website).text
+        soup = BeautifulSoup(text, features='lxml')
+        tables = soup.findAll("table", {"class":"cargoDynamicTable display"})
+        normal_moves_row_caption = tables[0].find("thead").findAll("th")
+        normal_moves = tables[0].find("tbody").findAll("td")
+
+        print(len(normal_moves))
+        print(len(normal_moves_row_caption))
+        print(normal_moves_row_caption)
+        breakpoint()
+        """
+        special_moves_row_caption = tables[1].find("thead").find("tr")
+        special_moves = tables[1].find("tbody").findAll("tr")
+        
+        overdrive_moves_row_caption = tables[2].find("thead").find("tr")
+        overdrive_moves = tables[2].find("tbody").findAll("tr")
+        
+        other_moves_row_caption = tables[3].find("thead").find("tr")
+        other_moves = tables[3].find("tbody").findAll("tr")
+        """
+        test = get_dictionary_from_keys(normal_moves_row_caption[1:])
+        breakpoint()
+        print(test.keys())
+        return test
+
+
+def get_dictionary_from_keys(uncleaned_html):
+    my_dic = OrderedDict()
+    for key in uncleaned_html:
+        breakpoint()
+        my_dic.setdefault(key.contents[0], "")
+
+    return my_dic
+
